3df6dc1ebe95ed733e51a3663f3ee87604b004c3
tk-ausaf <tkausaf@gmail.com>
2026-09-02 02:04:29 +0530

    Add notification feature with Cloudinary image upload
    Notifications can optionally carry an image, uploaded to Cloudinary
    and referenced by its secure URL. Left publicly readable/postable and
    unrelated to the Sudoku feature.

---
+++ src/main/java/com/ausaf/sudoku/repository/notification/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.ausaf.sudoku.repository.notification;
+
+import com.ausaf.sudoku.entity.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+}
